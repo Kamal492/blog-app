9c81fc9befda234d2aa1dca8e0c3db6d783f6290
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2023-08-18 23:57:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -69,9 +69,7 @@ app.use((req, res, next) => {
 });
 app.use(allowCrossDomain);
 
-app.get('*', (req, res) => {
-  res.sendFile('index.html'); // Adjust the path here
-});
+
 
 app.listen(PORT, () => {
     console.log("Server is running");

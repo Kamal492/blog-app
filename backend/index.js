664271d026aa9fc17b05fc5b277c43bfcab04ae6
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2023-08-19 10:06:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -68,10 +68,7 @@ const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 }
-app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-store');
-  next();
-});
+
 app.use(allowCrossDomain);
 
 
